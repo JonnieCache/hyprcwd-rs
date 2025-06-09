df045c6aad2e9d3daa494900c95921f518a20225
Jonathan Davies <jonnie@cleverna.me>
2025-06-09 19:08:22 +0100

    fix child process search

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use std::env;
 fn main() -> Result<()> {
     let working_dir = active_window_cwd()?;
     println!("{}", working_dir);
+
     Ok(())
 }
 
@@ -14,28 +15,21 @@ fn active_window_cwd() -> Result<String> {
     let active_window =
         Client::get_active()?.ok_or_else(|| anyhow::anyhow!("No active window found"))?;
     let window_pid = active_window.pid;
+
     let child_pid = newest_child_process(window_pid)?;
 
     process_cwd(child_pid).or_else(|_| home_dir())
 }
 
 fn newest_child_process(parent_pid: i32) -> Result<i32> {
-    let mut newest_pid = parent_pid;
-    let mut newest_start_time = 0u64;
     let all_processes = procfs::process::all_processes()?;
 
-    for process in all_processes.flatten() {
-        if let Ok(stat) = process.stat() {
-            if stat.ppid == parent_pid {
-                if stat.starttime > newest_start_time {
-                    newest_start_time = stat.starttime;
-                    newest_pid = process.pid;
-                }
-            }
-        }
-    }
-
-    Ok(newest_pid)
+    all_processes
+        .flatten()
+        .flat_map(|p| p.stat())
+        .filter(|p| p.ppid == parent_pid)
+        .max_by_key(|p| p.starttime)
+        .map_or(Ok(parent_pid), |p| Ok(p.pid))
 }
 
 fn process_cwd(pid: i32) -> Result<String> {
